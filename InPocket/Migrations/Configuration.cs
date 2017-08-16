bdc24dd579d674e9c4a92521a736445fb7c3d3aa
kaibin <kayupeace@gmail.com>
2017-08-16 14:20:51 +1000

    Enable Database Migration Add New Migration Folder Add New Database Annotations Performance Update Database

---
+++ InPocket/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace InPocket.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<InPocket.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "InPocket.Models.MovieDBContext";
+        }
+
+        protected override void Seed(InPocket.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- InPocket/Models/Movie.cs
+++ InPocket/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 // this is a tutorial model class, not realted to the project we going to do
 // Reference Link : https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-a-model
@@ -13,11 +14,33 @@ namespace InPocket.Models
 {
     public class Movie
     {
-        public int ID { get; set; }
+        public int ID { set; get; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
+
+        // Reference : https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/examining-the-edit-methods-and-edit-view
+        // set data annotations for database model
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        //        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        /**
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
+    **/
     }
 
     // in order to use entity framework database context, u need install NuGet Package 
@@ -25,6 +48,7 @@ namespace InPocket.Models
     // From the Tools menu click NuGet Package Manager
     // Click Package Manager Console
     // Enter:   Install-Package EntityFramework
+
     public class MovieDBContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
