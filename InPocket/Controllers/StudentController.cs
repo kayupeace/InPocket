71998b855531e19ad77bf066c2847bd1150925d8
kaibin <kayupeace@gmail.com>
2017-08-16 23:44:40 +1000

    Add PageList To Student List Page

--- InPocket/Controllers/StudentController.cs
+++ InPocket/Controllers/StudentController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using InPocket.DAL;
 using InPocket.Models;
+using PagedList;
 
 namespace InPocket.Controllers
 {
@@ -22,10 +23,23 @@ namespace InPocket.Controllers
             return View(db.Students.ToList());
         }
             **/
-        public ActionResult Index(string sortOrder, string searchString)
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
             var students = from s in db.Students
                            select s;
             if (!String.IsNullOrEmpty(searchString))
@@ -48,6 +62,9 @@ namespace InPocket.Controllers
                     students = students.OrderBy(s => s.LastName);
                     break;
             }
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
             return View(students.ToList());
         }
 
